14593907bb0ec66e7fecfbb7662c19750b990645
Michael Pinto <2000600@slu.edu.ph>
2019-03-17 22:01:24 +0800

    Added a revised admin

---
+++ Admin/revised admin/admin/branch.php
@@ -0,0 +1,25 @@
+<?php
+include 'template/branchhead.php';
+?>
+<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+    <div class="row">
+        <ol class="breadcrumb">
+            <li><a href="#">
+                <em class="fa fa-home"></em>
+                </a></li>
+            <li class="active">Branch Stock Request</li>
+        </ol>
+    </div><!--/.row-->
+
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Branch Stock Request</h1>
+        </div>
+    </div><!--/.row-->
+
+</div><!--/.row-->
+</div>	<!--/.main-->
+
+<?php
+include 'template/footer.php';
+?>

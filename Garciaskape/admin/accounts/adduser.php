f9b688f635cb1185e7989d6c2ec461727556d1eb
VictoriaBuse <35962189+VictoriaBendiii@users.noreply.github.com>
2019-04-19 20:16:35 +0800

    made changes in files in accounts folder

--- Garciaskape/admin/accounts/adduser.php
+++ Garciaskape/admin/accounts/adduser.php
@@ -3,52 +3,48 @@
 
 if (isset($_POST['add_user'])) {
 
-    $username = mysqli_real_escape_string($conn, $_POST['username']);
-    $password = mysqli_real_escape_string($conn, $_POST['password']);
-    $confirm_password = mysqli_real_escape_string($conn, $_POST['confirm_password']);
-    $lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
-    $firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
-    $middlename = mysqli_real_escape_string($conn, $_POST['middlename']);
-    $contact_number = mysqli_real_escape_string($conn, $_POST['contact_number']);
-    $usertype = mysqli_real_escape_string($conn, $_POST['usertype']);
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    $status = "Active";
-    $branchid;
-    if ($usertype == "sub-admin1") {
-        $branchid =1;
-        header('Location: accounts.php');
-    } else if ($usertype == "sub-admin2") {
-        $branchid =2;
-        header('Location: accounts.php');
-    } else {
-        $branchid =3;
-        header('Location: accounts.php');
-    }
-    if (empty($username)) { 
-        array_push($errors, "Username is required");
-    }
+  $username = mysqli_real_escape_string($conn, $_POST['username']);
+  $password = mysqli_real_escape_string($conn, $_POST['password']);
+  $confirm_password = mysqli_real_escape_string($conn, $_POST['confirm_password']);
+  $lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
+  $firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
+  $middlename = mysqli_real_escape_string($conn, $_POST['middlename']);
+  $contact_number = mysqli_real_escape_string($conn, $_POST['contact_number']);
+  $usertype = mysqli_real_escape_string($conn, $_POST['usertype']);
+  $email = mysqli_real_escape_string($conn, $_POST['email']);
+  $status = "Active";
+  $branchid;
+  if ($usertype == "sub-admin1") {
+    $branchid =1;
+    
+  } else if ($usertype == "sub-admin2") {
+    $branchid =2;
+    
+  } else {
+    $branchid =3;
+    
+  }
+  if (empty($username)) { 
+    array_push($errors, "Username is required");
+  }
+  
+  if (empty($password)) {
+    array_push($errors, "Password is required"); 
+  }
 
-    if (empty($password)) {
-        array_push($errors, "Password is required"); 
-    }
+  if ($password != $confirm_password) {
+    array_push($errors, "The two passwords do not match");
+  }
 
-    if ($password != $confirm_password) {
-        array_push($errors, "The two passwords do not match");
-    }
+  // for encryption
+  
+  $ecnrypt_password = base64_encode($password);
 
-    $hash_password = password_hash($password, PASSWORD_DEFAULT);
-
-
-
-    $sql = "INSERT INTO accounts (username, email, password, user_type, firstname, middlename, lastname, contact_number, status, branchid) 
-          VALUES('$username', '$email', '$hash_password', '$usertype','$firstname', '$middlename', '$lastname', '$contact_number',  '$status', '$branchid')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-    $conn->close();
+  	$sql = "INSERT INTO accounts (username, email, password, user_type, firstname, middlename, lastname, contact_number, status, branchid) 
+          VALUES('$username', '$email', '$ecnrypt_password', '$usertype','$firstname', '$middlename', '$lastname', '$contact_number',  '$status', '$branchid')";
+    
+    mysqli_query($conn, $sql);
+    header("location: addaccount.php");
 }
 
 ?>
--- Garciaskape/admin/accounts/update.php
+++ Garciaskape/admin/accounts/update.php
@@ -8,49 +8,32 @@ $user_type='';
 
 if (isset($_GET['activate'])){
 
-    $id = $_GET['activate'];
-    $sql_query = "UPDATE accounts SET status='Active' WHERE accountid=$id";
-    mysqli_query($conn, $sql_query);
-    header("location: accounts.php");
+	$id = $_GET['activate'];
+	$sql_query = "UPDATE accounts SET status='Active' WHERE accountid=$id";
+	mysqli_query($conn, $sql_query);
+   header("location: accounts.php");
 
 } else if (isset($_GET['deactivate'])){
 
-    $id = $_GET['deactivate'];
-    $sql_query ="UPDATE accounts SET status='Deactivated' WHERE accountid=$id";
-    mysqli_query($conn, $sql_query);
+	$id = $_GET['deactivate'];
+	$sql_query ="UPDATE accounts SET status='Deactivated' WHERE accountid=$id";
+	mysqli_query($conn, $sql_query);
     header("location: accounts.php");
 }
 
 else if (isset($_GET['delete'])){
 
-    $id = $_GET['delete'];
-    $sql_query ="DELETE FROM accounts WHERE accountid=$id";
-    mysqli_query($conn, $sql_query);
-    $_SESSION['message'] = "Account has been deleted!";
-    $_SESSION['msg_type'] = "danger";
+	$id = $_GET['delete'];
+	$sql_query ="DELETE FROM accounts WHERE accountid=$id";
+	mysqli_query($conn, $sql_query);
+	$_SESSION['message'] = "Account has been deleted!";
+	$_SESSION['msg_type'] = "danger";
 
     header("location: accounts.php");
 }
-
-else if (isset($_GET['edit'])){
-
-    $id = $_GET['edit'];
-    $sql_query ="SELECT * FROM accounts WHERE accountid=$id";
-    $result = mysqli_query($conn, $sql_query);
-
-    if (count($result) == 1) {
-        $row = mysqli_fetch_array($result);
-        $firstname= $row['firstname'];
-        $lastname = $row['lastname'];
-        $user_type = $row['user_type'];
-    }
-    mysqli_query($conn, $sql_query);
-    header("location: accounts.php");
-}
-
 
 if(isset($_POST['edit_account'])){
-    $id = mysqli_real_escape_string($conn, $_POST['accountid']);
+	$id = mysqli_real_escape_string($conn, $_POST['accountid']);
     $username = mysqli_real_escape_string($conn, $_POST['username']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
     $confirm_password = mysqli_real_escape_string($conn, $_POST['confirm_password']);
@@ -61,25 +44,25 @@ if(isset($_POST['edit_account'])){
     $email = mysqli_real_escape_string($conn, $_POST['email']);
     if ($password != $confirm_password) {
         array_push($errors, "The two passwords do not match");
-    }
-
-    $hash_password = password_hash($password, PASSWORD_DEFAULT);
-
-    $sql = "UPDATE accounts SET username='$username', 
-          password='$hash_password', 
+      }
+    
+      $ecnrypt_password = base64_encode($password);
+    
+          $sql = "UPDATE accounts SET username='$username', 
+          password='$ecnrypt_password', 
           firstname='$firstname',
           lastname='$lastname', 
           user_type='$usertype',
           contact_number='$contact_number',
           email='$email' 
 		  WHERE accountid='$id'";
-
-    mysqli_query($conn, $sql);
-    header("location: accounts.php");  
-
-
-
-    $conn->close();
-}
+        
+		mysqli_query($conn, $sql);
+		header("location: accounts.php");  
+ 
+    
+    
+        $conn->close();
+    }
 
 ?>

9623f991131932fe63e907ae5f14b189d19ef84f
John Jezreel Bugtong <2165321@slu.edu.ph>
2019-04-19 16:42:40 +0800

    Fixed the Request stock functionality

--- Garciaskape/subadmin1/request.php
+++ Garciaskape/subadmin1/request.php
@@ -139,19 +139,16 @@ th, td{
 				$status = "pending";
 
 				$sql_ord = "SELECT orderid from orders order by orderid desc limit 1";
-				$result = (int)$sql_ord;
-				$result++;
-
+				$result = mysqli_query($conn, $sql_ord);
+				$row = mysqli_num_rows($result);
+				$row = mysqli_fetch_array($result);
+				$res = $row['orderid'];
+				$res++;
 
 				$sql_sub = "INSERT INTO orders (orderid, stockid, productid, quantity, solditemid, deliveryid, supplierid, branchid, accountid, time, status)
-							VALUES ('$result', NULL, '$prodname', '$prodquan', NULL, NULL, NULL, '$branchid', '$accountid', SYSDATE(), '$status')";
+							VALUES ('$res', NULL, '$prodname', '$prodquan', NULL, NULL, NULL, '$branchid', '$accountid', SYSDATE(), '$status')";
 				
-				//mysqli_query($conn, $sql_sub);
-				if ($conn->query($sql_sub) === TRUE) {
-					echo "New record created successfully";
-				} else {
-					echo "Error: " . $sql_sub . "<br>" . $conn->error;
-				}
+				mysqli_query($conn, $sql_sub);
 			}
 
 		?>

d90a30913f2e7333655b8234b9ba35ca06abb8d9
Aaron Quitoriano <34297929+Rondimonigga@users.noreply.github.com>
2019-05-28 15:31:37 +0800

    Session manager file
    Do not modify

---
+++ Garciaskape/session.php
@@ -0,0 +1,20 @@
+<?php
+$IP = getenv ( "REMOTE_ADDR" );
+$connection = mysql_connect("localhost", "root", "");
+// Selecting Database
+$db = mysql_select_db("garciaspremiumcoffee", $connection);
+session_start();// Starting Session
+
+// Storing Session
+$user_check=$_SESSION['login_user'];
+
+// SQL Query To Fetch Complete Information Of User
+$ses_sql=mysql_query("select username from login where username='$user_check'", $connection);
+$row = mysql_fetch_assoc($ses_sql);
+$login_session =$row['username'];
+$login_session_type = $row['user_type'];
+if(!isset($login_session)){
+  mysql_close($connection); // Closing Connection
+  header('Location: index.php'); // Redirecting To Home Page
+}
+?>

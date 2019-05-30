5fdde6888f8c8a6864adbfd5882a0d30053c2654
Aaron Quitoriano <34297929+Rondimonigga@users.noreply.github.com>
2019-05-30 22:47:38 +0800

    Updated database connection
    Basic error handling

--- Garciaskape/includes/dbh.inc.php
+++ Garciaskape/includes/dbh.inc.php
@@ -3,8 +3,11 @@ $dbservername = "localhost";
 $dbusername = "root";
 $dbpassword = "";
 $dbname = "garciaspremiumcoffee";
+try{
+  $conn = mysqli_connect( $dbservername, $dbusername, $dbpassword, $dbname);
 
-$conn = mysqli_connect( $dbservername, $dbusername, $dbpassword, $dbname);
-
+} catch (mysqli_sql_exception $exc){
+    die("Can't connect to the database! \n" . $exc);
+}
 
 ?>

677229e2218e020add3624b88b1ad4eb7f7f47ad
Aaron <2162232@slu.edu.ph>
2019-05-30 23:49:17 +0800

    .htaccess files and exception handling

---
+++ Garciaskape/errors/error.php
@@ -0,0 +1,68 @@
+<?php
+$status = $_SERVER['REDIRECT_STATUS'];
+
+
+
+$codes = array(
+       403 => array('403 Forbidden', 'The server has refused to fulfill your request. '),
+       404 => array('404 Not Found', 'The document/file requested was not found on this server.'),
+       405 => array('405 Method Not Allowed', 'The method specified in the Request-Line is not allowed for the specified resource.'),
+       408 => array('408 Request Timeout', 'Your browser failed to send a request in the time allowed by the server.'),
+       500 => array('500 Internal Server Error', 'The request was unsuccessful due to an unexpected condition encountered by the server.'),
+       502 => array('502 Bad Gateway', 'The server received an invalid response from the upstream server while trying to fulfill the request.'),
+       504 => array('504 Gateway Timeout', 'The upstream server failed to send a request in the time allowed by the server.'),
+);
+
+$title = $codes[$status][0];
+$message = $codes[$status][1];
+if ($title == false || strlen($status) != 3) {
+       $message = 'Please supply a valid status code.';
+}
+
+?>
+<?php
+session_start();
+?>
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<style>
+    .error{
+        color: red;
+    }
+
+    #remember{
+        color: white;
+    }
+</style>
+
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Garcias Premium Coffee</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+        <link rel="stylesheet" href ="css/style.css" type="text/css">
+    </head>
+    <!--Steven Mangati-->
+    <body>
+        <div class="container h-100">
+          <h2 style="color:white; text-align: center;"> <?php echo $title;?> </h2><br>
+          <h3 style="color:white; text-align: center;"> Please go back to the <a href = "includes/logout.inc.php"> login page</a></h3>
+
+        </div>
+    </div>
+
+<script>
+    history.pushState(null, null, null);
+    window.addEventListener('popstate', function () {
+        history.pushState(null, null, null);
+    });
+</script>
+</body>
+</html>

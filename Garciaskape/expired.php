04c211bfc70a92a91ad4d651b9582fd0960d443b
Aaron Quitoriano <34297929+Rondimonigga@users.noreply.github.com>
2019-06-06 00:59:52 +0800

    Update expired.php

--- Garciaskape/expired.php
+++ Garciaskape/expired.php
@@ -1,6 +1,6 @@
 <?php
 function isLoginSessionExpired() {
-	$login_session_duration = 600; // 10 MINUTES DURATION
+	$login_session_duration = 1800; // 30 MINUTES DURATION
 	$current_time = time();
 	if(isset($_SESSION['loggedin_time']) and isset($_SESSION['login_user'])){
 		if(((time() - $_SESSION['loggedin_time']) > $login_session_duration)){

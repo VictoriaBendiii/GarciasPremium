5e77d0fc60fafaab705bcb9514b36061ab38b2fc
Aaron <2162232@slu.edu.ph>
2019-06-05 20:37:24 +0800

    sessions

--- Garciaskape/admin/supplier/process.php
+++ Garciaskape/admin/supplier/process.php
@@ -1,14 +1,14 @@
 <?php include '../includes/connection.php';
 ?>
 <?php
-session_start();
+
 if (isset($_POST['add_supplier'])) {
     $supplier_name = mysqli_real_escape_string($conn, $_POST['supplier_name']);
     $contact_person = mysqli_real_escape_string($conn, $_POST['contact_person']);
     $contact_number = mysqli_real_escape_string($conn, $_POST['contact_number']);
     $address = mysqli_real_escape_string($conn, $_POST['address']);
     $status = 'active';
-    
+
     //Check for duplicates
     $query_supplier = "SELECT * FROM supplier WHERE supplier_name='$supplier_name'";
     $result = mysqli_query($conn, $query_supplier);
@@ -19,7 +19,7 @@ if (isset($_POST['add_supplier'])) {
         header("location: addsupplier.php");
         die();
     }
-    
+
     $query_cp = "SELECT * FROM supplier WHERE supplier_contact_person='$contact_person'";
     $result = mysqli_query($conn, $query_cp);
     $cpcount=mysqli_num_rows($result);
@@ -29,7 +29,7 @@ if (isset($_POST['add_supplier'])) {
         header("location: addsupplier.php");
         die();
     }
-    
+
     $query_number = "SELECT * FROM supplier WHERE contact_number='$contact_number'";
     $result = mysqli_query($conn, $query_number);
     $numcount=mysqli_num_rows($result);
@@ -39,13 +39,13 @@ if (isset($_POST['add_supplier'])) {
         header("location: addsupplier.php");
         die();
     }
-    
-    $sql = "INSERT INTO supplier (supplier_name, supplier_contact_person, contact_number, address, status) 
+
+    $sql = "INSERT INTO supplier (supplier_name, supplier_contact_person, contact_number, address, status)
           VALUES('$supplier_name', '$contact_person', '$contact_number','$address', '$status')";
     mysqli_query($conn, $sql);
     $_SESSION['message']="Account of ".$supplier_name. " has been created!";
     $_SESSION['msg_type']="success";
-    header('location: addsupplier.php'); 
+    header('location: addsupplier.php');
     $conn->close();
 }
 ?>

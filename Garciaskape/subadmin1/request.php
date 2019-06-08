733cb9da4f659ab857ac364226396b0d94cf2739
Aaron Quitoriano <34297929+Rondimonigga@users.noreply.github.com>
2019-06-08 14:33:45 +0800

    Confirmation on Modal

--- Garciaskape/subadmin1/request.php
+++ Garciaskape/subadmin1/request.php
@@ -121,7 +121,7 @@ $page = 'request'; ?>
                                 <br>
                                 <tr>
                                     <td> <input type="button" onclick="cloneRow(event)" name="add" id="add" value="Add" class="btn btn-secondary"/> </td>
-                                    <td> <button type="submit" class="btn btn-primary" name="sub" id="sub">Submit</button> </td>
+                                    <td> <button type="submit" class="btn btn-primary" name="sub" id="sub" onclick = "return confirm('Are you sure with the request?')">Submit</button> </td>
                                 </tr>
                             </table>
                         </form>
